6f4bb43b6aae674541effd725dae44486b23309c
Dino Francisco <dinillo22@gmail.com>
2024-01-24 08:11:26 -0300

    branch 16.0.1

--- __manifest__.py
+++ __manifest__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+<<<<<<< Updated upstream
 ###################################################################################
 #
 #    Cybrosys Technologies Pvt. Ltd.
@@ -24,13 +25,40 @@
     'version': '12.0.1.1.1',
     'summary': """Integrating Biometric Device With HR Attendance (Face + Thumb)""",
     'description': 'This module integrates Odoo with the biometric device(Model: ZKteco uFace 202)',
+=======
+{
+    'name': "Zkteco, eSSL, Cams Biometrics Integration Module with HR Attendance",
+    'summary': """Receives the attendance/punches from the biometric devices and updates in hr.attendance module of odoo server.""",
+    'description': """
+        The module is developed based on the Cams biometric web api as documented at https://camsunit.com/application/biometric-web-api.html. It receives the biometric attendance on realtime and integrates with hr.attendance module. 
+        
+	It supports all the cams biometrics machines listed at https://camsunit.com/product/home.html
+        It alos supports 
+        	ZKTeco, 
+        	eSSL, 
+        	Identix, 
+        	BioMax 
+        	and more biometric machines provided they are verified at https://developer.camsunit.com/
+        
+        Module requires valid API license as listed at https://camsunit.com/application/biometric-web-api.html#api_cost
+        
+    """,
+
+    'author': "Cams Biometrics",
+>>>>>>> Stashed changes
     'category': 'Generic Modules/Human Resources',
-    'author': 'Cybrosys Techno Solutions, Mostafa Shokiel',
-    'company': 'Cybrosys Techno Solutions',
-    'website': "https://www.cybrosys.com",
-    'depends': ['base_setup', 'hr_attendance'],
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'company': 'Cams Biometrics',
+    'website': "https://www.camsunit.com",
+    'depends': ['hr','hr_attendance'],
+    'installable': True,
+    'images':[
+        'static/description/banner.png',
+        ],
     'data': [
         'security/ir.model.access.csv',
+<<<<<<< Updated upstream
         'views/zk_machine_view.xml',
         'views/zk_machine_attendance_view.xml',
         'data/download_data.xml'
@@ -42,4 +70,10 @@
     'installable': True,
     'auto_install': False,
     'application': False,
+=======
+        'views/views.xml',
+        'views/config.xml',
+        'views/inherited_employee_view.xml',
+    ]
+>>>>>>> Stashed changes
 }
